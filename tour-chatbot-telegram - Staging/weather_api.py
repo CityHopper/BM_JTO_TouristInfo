de9b322dc9ad92b532260385cf5df30c263b7085
CityHopper <43904953+CityHopper@users.noreply.github.com>
2020-06-14 18:50:39 +0900

    200614

--- tour-chatbot-telegram - Staging/weather_api.py
+++ tour-chatbot-telegram - Staging/weather_api.py
@@ -6,36 +6,44 @@ import json
 from datetime import datetime
 
 def weather_today():
-    today = datetime.today().strftime('%Y%m%d')
-    hour = datetime.now().hour - 1
-    url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
-    queryParams = '?' + urlencode({
-        quote_plus('serviceKey'): '3Cz8y44yxy3z1fY6VWfk8m8YDvZSMrKJWeJ7uM%2F7ZbPJpXDQ1ljNxEDplkQsyGf1wA9W98rnutPWt76IQV1Tmg%3D%3D',
-        quote_plus('pageNo'): '1',
-        quote_plus('numOfRows'): '10',
-        quote_plus('dataType'): 'JSON',
-        quote_plus('base_date'): today,
-        quote_plus('base_time'): str(hour)+'00',
-        quote_plus('nx'): '53', # 제주시 격자 X값=53, 서귀포시 격자 X값=52
-        quote_plus('ny'): '38', # 제주시 격자 Y값=38, 서귀포시 격자 Y값=33
-    })
+    hour = datetime.now().hour
+    while True:
+        try:
+            today = datetime.today().strftime('%Y%m%d')
+            url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
+            queryParams = '?' + urlencode({
+                quote_plus('serviceKey'): '3Cz8y44yxy3z1fY6VWfk8m8YDvZSMrKJWeJ7uM%2F7ZbPJpXDQ1ljNxEDplkQsyGf1wA9W98rnutPWt76IQV1Tmg%3D%3D',
+                quote_plus('pageNo'): '1',
+                quote_plus('numOfRows'): '10',
+                quote_plus('dataType'): 'JSON',
+                quote_plus('base_date'): today,
+                quote_plus('base_time'): str(hour)+'00',
+                quote_plus('nx'): '53', # 제주시 격자 X값=53, 서귀포시 격자 X값=52
+                quote_plus('ny'): '38', # 제주시 격자 Y값=38, 서귀포시 격자 Y값=33
+            })
 
-    print(url + unquote(queryParams))
-    request = Request(url + unquote(queryParams))
-    response_body = urlopen(request).read()
-    data = json.loads(response_body)
-    result = pd.DataFrame(data['response']['body']['items']['item'])
-    # print(result)
+            print(url + unquote(queryParams))
+            request = Request(url + unquote(queryParams))
+            response_body = urlopen(request).read()
+            data = json.loads(response_body)
+            result = pd.DataFrame(data['response']['body']['items']['item'])
+            print(result)
+            pop = int(result[result['category'] == 'POP']['fcstValue'])
+            pty = int(result[result['category'] == 'PTY']['fcstValue'])
+            # r06 = int(result[result['category'] == 'R06']['fcstValue'])
+            reh = int(result[result['category'] == 'REH']['fcstValue'])
+            # s06 = int(result[result['category'] == 'S06']['fcstValue'])
+            sky = int(result[result['category'] == 'SKY']['fcstValue'])
+            t3h = int(result[result['category'] == 'T3H']['fcstValue'])
+            basetime = hour
+            return pop, pty, reh, sky, t3h, basetime
+        except KeyError:
+            hour -= 1
+            continue
 
-    pop = int(result[result['category'] == 'POP']['fcstValue'])
-    pty = int(result[result['category'] == 'PTY']['fcstValue'])
-    r06 = int(result[result['category'] == 'R06']['fcstValue'])
-    reh = int(result[result['category'] == 'REH']['fcstValue'])
-    s06 = int(result[result['category'] == 'S06']['fcstValue'])
-    sky = int(result[result['category'] == 'SKY']['fcstValue'])
-    t3h = int(result[result['category'] == 'T3H']['fcstValue'])
 
-    return pop, pty, r06, reh, s06, sky, t3h
+weather_today()
+
 '''
 - 동네예보 결과값 코드 설명(단위)
 POP 강수확률 (%)
